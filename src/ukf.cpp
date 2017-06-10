acab5dd61f0fa6e6a2466a49aefee56225db539f
Igor Molina <canutito1@hotmail.com>
2017-06-11 00:33:47 +0200

    Changes to ukf.cpp

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -51,6 +51,28 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  is_initialized_ = false;
+  previous_timestamp_ = 0;
+
+
+  ///* State dimension
+  int n_x_ = 5;
+
+  ///* Augmented state dimension
+  int n_aug_ = 2*n_x_+1;
+
+  ///* Sigma point spreading parameter
+  double lambda_ = 3 - n_aug_;
+
+  ///* Weights of sigma points
+  VectorXd weights_ = VectorXd(2*n_aug_+1);
+  double weight_0_ = lambda_/(lambda_+n_aug_);
+  weights_(0) = weight_0_;
+  for (int i=1; i<2*n_aug_+1; i++) {  
+    double weight_ = 0.5/(n_aug_+lambda_);
+    weights_(i) = weight_;
+  }
+
 }
 
 UKF::~UKF() {}
@@ -66,6 +88,63 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+
+  /*****************************************************************************
+   *  Initialization
+   ****************************************************************************/
+  if (!is_initialized_) {
+
+    // first measurement
+    cout << "UKF: " << endl;
+
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      /**
+      Convert radar from polar to cartesian coordinates and initialize state.
+      */
+      ukf.x_ << measurement_pack.raw_measurements_[0]*cos(measurement_pack.raw_measurements_[1]),
+                 measurement_pack.raw_measurements_[0]*sin(measurement_pack.raw_measurements_[1]), 0, 0; // x, y, vx, vy
+
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      /**
+      Initialize state if first measurement coming from Lidar.
+      */
+      ukf.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+
+    }
+
+    previous_timestamp_ = measurement_pack.timestamp_;
+    // done initializing, no need to predict or update
+    is_initialized_ = true;
+    return;
+  }
+  
+  /*****************************************************************************
+   *  Prediction
+   ****************************************************************************/
+
+  //compute the time elapsed between the current and previous measurements
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+
+  ukf.Prediction(dt);
+
+  /*****************************************************************************
+   *  Update Step
+   ****************************************************************************/
+
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    // Radar updates
+
+
+      ukf.UpdateRadar(measurement_pack.raw_measurements_);
+  } else {
+    // Laser updates
+
+
+      ukf.UpdateLidar(measurement_pack.raw_measurements_);
 }
 
 /**
@@ -80,6 +159,87 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+
+  ///* Weights of sigma points
+  /*
+  VectorXd weights_ = VectorXd(2*n_aug_+1);
+  double weight_0_ = lambda_/(lambda_+n_aug_);
+  weights_(0) = weight_0_;
+  for (int i=1; i<2*n_aug_+1; i++) {  
+    double weight_ = 0.5/(n_aug_+lambda_);
+    weights_(i) = weight_;
+  */
+  //radar measurement noise standard deviation radius in m
+  double std_radr = 0.3;
+
+  //radar measurement noise standard deviation angle in rad
+  double std_radphi = 0.0175;
+
+  //radar measurement noise standard deviation radius change in m/s
+  double std_radrd = 0.1;
+
+  //create example matrix with predicted sigma points
+  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+  Xsig_pred <<
+         5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
+           1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
+          2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
+         0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
+          0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
+
+  //create matrix for sigma points in measurement space
+  MatrixXd Zsig = MatrixXd(n_z, 2 * n_aug + 1);
+
+/*******************************************************************************
+ * Student part begin
+ ******************************************************************************/
+
+  //transform sigma points into measurement space
+  for (int i = 0; i < 2 * n_aug + 1; i++) {  //2n+1 simga points
+
+    // extract values for better readibility
+    double p_x = Xsig_pred(0,i);
+    double p_y = Xsig_pred(1,i);
+    double v  = Xsig_pred(2,i);
+    double yaw = Xsig_pred(3,i);
+
+    double v1 = cos(yaw)*v;
+    double v2 = sin(yaw)*v;
+
+    // measurement model
+    Zsig(0,i) = sqrt(p_x*p_x + p_y*p_y);                        //r
+    Zsig(1,i) = atan2(p_y,p_x);                                 //phi
+    Zsig(2,i) = (p_x*v1 + p_y*v2 ) / sqrt(p_x*p_x + p_y*p_y);   //r_dot
+  }
+
+  //mean predicted measurement
+  VectorXd z_pred = VectorXd(n_z);
+  z_pred.fill(0.0);
+  for (int i=0; i < 2*n_aug+1; i++) {
+      z_pred = z_pred + weights(i) * Zsig.col(i);
+  }
+
+  //measurement covariance matrix S
+  MatrixXd S = MatrixXd(n_z,n_z);
+  S.fill(0.0);
+  for (int i = 0; i < 2 * n_aug + 1; i++) {  //2n+1 simga points
+    //residual
+    VectorXd z_diff = Zsig.col(i) - z_pred;
+
+    //angle normalization
+    while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
+    while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
+
+    S = S + weights(i) * z_diff * z_diff.transpose();
+  }
+
+  //add measurement noise covariance matrix
+  MatrixXd R = MatrixXd(n_z,n_z);
+  R <<    std_radr*std_radr, 0, 0,
+          0, std_radphi*std_radphi, 0,
+          0, 0,std_radrd*std_radrd;
+  S = S + R;
+
 }
 
 /**
